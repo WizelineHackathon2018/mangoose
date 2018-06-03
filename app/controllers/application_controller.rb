1a9202a89ae8eb3fa8c7e275451bb1e226565ec3
Raul Migdonio Rodriguez Te <raulmigdonio@gmail.com>
2018-06-03 07:38:51 -0500

    add endpoint for test spellcheck a text with multiple dictionaries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  aff_path = Rails.root + 'lib/dictionaries/index.aff'
+  base_dic_path = Rails.root + 'lib/dictionaries/base.dic'
+  dev_dic_path = Rails.root + 'lib/dictionaries/developer.dic'
+  designer_dic_path = Rails.root + 'lib/dictionaries/designer.dic'
+
+  $sp1 = Hunspell.new aff_path.to_s, base_dic_path.to_s
+  $sp2 = Hunspell.new aff_path.to_s, designer_dic_path.to_s
+  $sp3 = Hunspell.new aff_path.to_s, dev_dic_path.to_s
 end
